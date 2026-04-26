b9ba8d4c1c6e399a7535fa9d6ce7caef415217d4
vyeos <rudranpatel0980@gmail.com>
2026-04-26 22:08:56 +0530

    add: schema

---
+++ packages/db/index.ts
@@ -0,0 +1,11 @@
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "@prisma/client/extension";
+
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL!,
+});
+
+export const db = new PrismaClient({
+  adapter,
+});
+
