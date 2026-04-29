9d568eb3e95327209101c192bb00b834b5904678
vyeos <rudranpatel0980@gmail.com>
2026-04-29 11:08:54 +0530

    feat: calling the classes

--- apps/api-backend/src/index.ts
+++ apps/api-backend/src/index.ts
@@ -2,10 +2,12 @@ import { Elysia } from "elysia";
 import bearer from "@elysia/bearer";
 import { Conversation, LlmResponse } from "./types";
 import { db } from "db";
+import { Gemini } from "./llms/Gemini";
+import { Claude } from "./llms/Claude";
+import { OpenAi } from "./llms/Openai";
 
 const app = new Elysia()
   .use(bearer())
-  //.use(openapi());
   .post(
     "/api/v1/chat/completions",
     async ({ status, bearer: apiKey, body }) => {
@@ -48,9 +50,49 @@ const app = new Elysia()
       });
 
       const provider =
-        providerDb[Math.floor(Math.random() * providerDb.length)];
+        providerDb[Math.floor(Math.random() * providerDb.length)].provider;
 
       let response: LlmResponse | null = null;
+
+      if (provider.name == "Google")
+        response = await Gemini.chat(providerModelName, body.messages);
+      else if (provider.name == "Claude")
+        response = await Claude.chat(providerModelName, body.messages);
+      else if (provider.name == "OpenAI")
+        response = await OpenAi.chat(providerModelName, body.messages);
+
+      if (!response)
+        return status(403, { message: "No provider found for this model" });
+
+      const creditsUsed =
+        (response.inputTokensConsumed * provider.inputTokenCost +
+          response.outputTokensConsumed * provider.outputTokenCost) /
+        10;
+      console.log(creditsUsed);
+      const res = await db.user.update({
+        where: {
+          id: apiKeyDb.user.id,
+        },
+        data: {
+          credits: {
+            decrement: creditsUsed,
+          },
+        },
+      });
+      console.log(res);
+      const res2 = await db.apiKey.update({
+        where: {
+          apiKey: apiKey,
+        },
+        data: {
+          creditsConsumed: {
+            increment: creditsUsed,
+          },
+        },
+      });
+      console.log(res2);
+
+      return response;
     },
 
     { body: Conversation },
